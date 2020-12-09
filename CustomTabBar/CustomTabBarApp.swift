062a408ee2a8dba86d1e37358a9133ccd9c9b9d4
tanvir <tanvirgeek@gmail.com>
2020-12-09 11:12:03 +0600

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by MD Tanvir Alam on 9/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
